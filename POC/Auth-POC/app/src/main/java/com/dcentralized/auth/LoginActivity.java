7a898e9121f398637ef65a341437b3ffee0a7f07
Tom de Wildt <t.dewildt@student.fontys.nl>
2017-12-08 16:05:45 +0100

    Working on Auth-POC

--- POC/Auth-POC/app/src/main/java/com/dcentralized/auth/LoginActivity.java
+++ POC/Auth-POC/app/src/main/java/com/dcentralized/auth/LoginActivity.java
@@ -1,31 +1,50 @@
 package com.dcentralized.auth;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 /**
- * https://github.com/pingidentity/Android-AppAuth-Sample-Application
+ * http://progur.com/2016/10/how-to-use-reddit-oauth2-in-android-apps.html
  */
 public class LoginActivity extends AppCompatActivity {
-    private Button btnLogin;
+    private static final String AUTH_URL = "https://www.reddit.com/api/v1/authorize.compact?client_id=%s&response_type=code&state=%s&redirect_uri=%s&duration=permanent&scope=identity";
+    private static final String CLIENT_ID = "fyuIYxjWBHIfGA";
+    private static final String REDIRECT_URI = "com.dcentralized.auth://callback";
+    private static final String STATE = "MY_RANDOM_STRING_1";
+    private static final String ACCESS_TOKEN_URL = "https://www.reddit.com/api/v1/access_token";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onButtonLoginClick(view);
-            }
-        });
     }
 
-    private void onButtonLoginClick(View view) {
+    public void onButtonLoginClick(View view) {
+        String url = String.format(AUTH_URL, CLIENT_ID, STATE, REDIRECT_URI);
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(this, Uri.parse(url));
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(getIntent()!=null && getIntent().getAction().equals(Intent.ACTION_VIEW)) {
+            Uri uri = getIntent().getData();
+            if(uri.getQueryParameter("error") != null) {
+                String error = uri.getQueryParameter("error");
+                Log.e("LoginActivity", "An error has occurred : " + error);
+            } else {
+                String state = uri.getQueryParameter("state");
+                if(state.equals(STATE)) {
+                    String code = uri.getQueryParameter("code");
+                }
+            }
+        }
     }
 }
