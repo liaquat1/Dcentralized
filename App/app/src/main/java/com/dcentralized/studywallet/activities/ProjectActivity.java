ee4d91859af3bcda368c18c4a7ce49e14c2b9a62
Liwus <l.hu@student.fontys.nl>
2018-01-12 01:07:11 +0100

    I give up, f**** the toolbar title

--- App/app/src/main/java/com/dcentralized/studywallet/activities/ProjectActivity.java
+++ App/app/src/main/java/com/dcentralized/studywallet/activities/ProjectActivity.java
@@ -1,10 +1,18 @@
 package com.dcentralized.studywallet.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.dcentralized.studywallet.R;
 import com.dcentralized.studywallet.models.Project;
+
+import java.text.SimpleDateFormat;
 
 public class ProjectActivity extends AppCompatActivity {
     private Project project;
@@ -12,7 +20,16 @@ public class ProjectActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project2);
         project = (Project)getIntent().getSerializableExtra("project");
-        setContentView(R.layout.activity_project);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        ((TextView)findViewById(R.id.tvProjectDate)).setText(formatter.format(project.getTime()));
+        ((TextView)findViewById(R.id.tvProjectCoins)).setText(String.valueOf(project.getReward()));
+        ((TextView)findViewById(R.id.tvProjectDescription)).setText(project.getDescription());
+        ((TextView)findViewById(R.id.tvProjectDifficulty)).setText(project.getName()); //Here comes the difficulty
+        ((ImageView) findViewById(R.id.ivLogo)).setImageDrawable(project.getLogo(this));
+        getSupportActionBar().setTitle(project.getName());
     }
 }
